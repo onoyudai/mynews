3c69557ca06b4eba024f35a7d8356ab873ba8819
onoyudai <yudai02140214@outlook.jp>
2021-10-09 04:44:03 +0000

    fifth  commit

---
+++ resources/views/layouts/profile.blade.php
@@ -0,0 +1,22 @@
+{{-- 次の1行を削除 --}}
+        <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
+        {{-- 次の1行を追記 --}}
+        <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
+        
+        {{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'ニュースの新規作成')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create'); # 追記
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth'); // 追記
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); // 追記
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+});
